e87dd4892faa85719fe714c425ef5909176b2ea5
Antoine Debroye <ant.debroye@gmail.com>
2022-09-10 17:59:01 +0200

    Add the bloc engine for authentication

--- lib/bloc/bloc/auth_state.dart
+++ lib/bloc/bloc/auth_state.dart
@@ -1,10 +1,27 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthState extends Equatable {
-  const AuthState();
+abstract class AuthState extends Equatable {}
+
+class Loading extends AuthState {
+  @override
+  List<Object?> get props => [];
 }
 
-class AuthInitial extends AuthState {
+// When the user is authenticated the state is changed to Authenticated.
+class Authenticated extends AuthState {
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+// This is the initial state of the bloc. When the user is not authenticated the state is changed to Unauthenticated.
+class UnAuthenticated extends AuthState {
+  @override
+  List<Object?> get props => [];
+}
+// If any error occurs the state is changed to AuthError.
+class AuthError extends AuthState {
+  final String error;
+
+  AuthError(this.error);
+  @override
+  List<Object?> get props => [error];
 }
--- lib/bloc/bottom_nav_bar_cubit.dart
+++ lib/bloc/bottom_nav_bar_cubit.dart
@@ -1,8 +1,7 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-part 'bottom_nav_bar_state.dart';
+class BottomNavBarCubit extends Cubit<int> {
+  BottomNavBarCubit() : super(0);
 
-class BottomNavBarCubit extends Cubit<BottomNavBarState> {
-  BottomNavBarCubit() : super(BottomNavBarInitial());
+  void selectTab(int index) => emit(index);
 }
